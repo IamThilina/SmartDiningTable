3d8b176aed56fba8149eee321d442356ad5091d5
Yasara123 <yasaradiss101@gmail.com>
2016-11-11 21:14:39 +0530

    adding pay

--- app/src/main/java/com/example/thilina/iothack/AfterPayed.java
+++ app/src/main/java/com/example/thilina/iothack/AfterPayed.java
@@ -5,15 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class Pay extends AppCompatActivity {
+public class AfterPayed extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pay);
+        setContentView(R.layout.activity_after_payed);
     }
-    public void launchAfterPayed(View view) {
-        Intent intent = new Intent(this, AfterPayed.class);
+    public void launchBacktoMenu(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
