71be3a00ff45b5e79765892863464fe5ac988c6f
Iamthilina <333thilina@gmail.com>
2016-11-11 18:04:26 +0530

    first commit

---
+++ app/src/main/java/com/example/thilina/iothack/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.thilina.iothack;
+
+import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void launchCamera(View view) {
+        Intent intent = new Intent(this, Menu.class);
+        startActivity(intent);
+    }
+}
